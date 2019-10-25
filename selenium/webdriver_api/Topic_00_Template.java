bb4339cd660b13b98a58c1bc4f6d8177be6c0617
hluong <hluong@VNMDTRD-LHUONG.dl.net>
2019-10-25 14:07:31 +0700

    Add testclass template

---
+++ selenium/webdriver_api/Topic_00_Template.java
@@ -0,0 +1,45 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_00_Template {
+	//Khai báo 1 cái biến driver đại diện cho Selenium Webdriver
+	WebDriver driver;
+
+	//Pre - condition 
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	
+	@Test
+	public void TC_01_() {
+		driver.get("");
+	}
+
+	@Test
+	public void TC_02_() {
+		driver.get("");
+	}
+
+		
+    // Post - Condition
+	@AfterClass
+	public void afterClass() {
+		// Tắt trình duyệt
+		driver.close();
+	}
+
+}
