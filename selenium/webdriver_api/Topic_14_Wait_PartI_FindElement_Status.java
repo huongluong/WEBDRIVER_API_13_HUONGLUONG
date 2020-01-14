cb815d41a4efe3cade2836cbe1095b191464b7f2
hluong <hluong@VNMDTRD-LHUONG.dl.net>
2020-01-14 16:25:26 +0700

    Add Topic 14

---
+++ selenium/webdriver_api/Topic_14_Wait_PartI_FindElement_Status.java
@@ -0,0 +1,43 @@
+package webdriver_api;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_14_Wait_PartI_FindElement_Status {
+	//Khai báo 1 cái biến driver đại diện cho Selenium Webdriver
+	WebDriver driver;
+
+	//Pre - condition 
+	@BeforeClass
+	public void beforeClass() {
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+	}
+
+	
+	@Test
+	public void TC_01_() {
+		driver.get("");
+	}
+
+	@Test
+	public void TC_02_() {
+		driver.get("");
+	}
+
+		
+    // Post - Condition
+	@AfterClass
+	public void afterClass() {
+		// Tắt trình duyệt
+		driver.close();
+	}
+
+}
