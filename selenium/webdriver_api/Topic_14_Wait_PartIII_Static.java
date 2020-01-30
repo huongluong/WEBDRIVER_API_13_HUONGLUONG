6fd1e988b45f77069316f32daeaa408c7be293b1
hluong <hluong@VNMDTRD-LHUONG.dl.net>
2020-01-30 13:48:00 +0700

    Update Static wait

--- selenium/webdriver_api/Topic_14_Wait_PartIII_Static.java
+++ selenium/webdriver_api/Topic_14_Wait_PartIII_Static.java
@@ -1,7 +1,9 @@
 package webdriver_api;
 
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -29,8 +31,20 @@ public class Topic_14_Wait_PartIII_Static {
 
 	
 	@Test
-	public void TC_01_() {
-		driver.get("");
+	public void TC_01_Static() throws Exception {
+		driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account");
+		System.out.println("Start sleep - " + getCurrentTime());
+		//1000ms = 1s
+		//Case 1- Nếu như page được load xong trong 3s thì sẽ mất 2s bị lãng phí
+		//Case 2- Nếu như page được load xong trong 10s hoặc hơn thì bị thiếu thời gian -> fail testcase
+		// Ko flexible
+		// 1 testcase = 30 steps x 1s = 30s
+		// 300 testcases x 30 steps x 1s = 9000s = 150m = 2.5hrs
+		// 300 testcases x 3 browsers = 2.5hrs x 3 = 7.5hrs
+		// Special case : Internet Explorer
+		Thread.sleep(5000);
+		System.out.println("End sleep - " + getCurrentTime());
+		driver.findElement(By.id("search_query_top")).sendKeys("Automation");
 	}
 
 	@Test
@@ -38,7 +52,10 @@ public class Topic_14_Wait_PartIII_Static {
 		driver.get("");
 	}
 
-		
+	  public String getCurrentTime() {
+	    	Date date = new Date();
+	    	return date.toString();
+	    }	
     // Post - Condition
 	@AfterClass
 	public void afterClass() {
