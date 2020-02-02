ed85294fb4a18d0e0ffb27a870611bb6da208434
Huong Luong <huongltd.fsoft@gmail.com>
2020-02-02 11:02:39 +0700

    Update

--- selenium/testng_framework/Part_V_Loop.java
+++ selenium/testng_framework/Part_V_Loop.java
@@ -4,13 +4,11 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class Part_V_Loop {
